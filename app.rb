ebae3e70d1a1c00b64ea9dd7630d106f98ba8eff
Matt Wright <mttwright@gmail.com>
2020-10-18 20:17:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,23 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is ham sandwich"
+  end
+  
+  get '/hometown' do
+    "My hometown is McChicken"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is"
+  end
+  
+  
+  
 end
